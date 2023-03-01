ad80ac20dfff48684e98c12b79f0ebb60e052b17
jlzg1987 <jlzg1987@hotmail.com>
2023-02-28 23:11:35 -0500

    update

--- src/routers/toDo.routes.js
+++ src/routers/toDo.routes.js
@@ -1,12 +1,12 @@
 const {Router} = require('express');
-const todos = require('../models/toDo.models');
+const toDo = require('../models/toDo.models');
 
 const router = Router();
 
 router.get("/api/v1/todo", async(req,res)=>{
     try{
-        const todo = await todos.findAll({
-          atributes:['ide','title','descrption', 'status']   
+        const todo = await toDo.findAll({
+          atributes:['id','title','description', 'status']   
         });
        
         res.json(todo);
@@ -21,7 +21,7 @@ router.get("/api/v1/todo", async(req,res)=>{
     try {
       const newUser = req.body;
       console.log(newUser);
-      const result = await User.create(newUser);
+      const result = await toDo.create(newUser);
       res.status(201).send(result);
     } catch (error) {
       res.status(400).json(error);
@@ -32,7 +32,7 @@ router.get("/api/v1/todo", async(req,res)=>{
     try {
       const { id } = req.params;
       const data = req.body;
-      await User.update(data, {
+      await toDo.update(data, {
         where: { id },
       });
       res.status(204).send();
@@ -45,7 +45,7 @@ router.get("/api/v1/todo", async(req,res)=>{
   router.delete("/api/v1/todo/:id", async (req, res) => {
     try {
       const { id } = req.params; // objeto
-      await User.destroy({
+      await toDo.destroy({
         where: { id }, // id: id
       });
       res.status(204).send();
